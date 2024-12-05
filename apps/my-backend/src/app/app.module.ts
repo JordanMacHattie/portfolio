d5b453d6cae8e5419100ee6757aa04a71fc953e7
Jordan MacHattie <jwmhattie@gmail.com>
2024-12-05 17:18:23 -0400

    module setup - users

--- apps/my-backend/src/app/app.module.ts
+++ apps/my-backend/src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 
+import { UserService } from '@user/user.service';
+import { UserController } from '@user/user.controller';
+import { DatabaseModule } from '@database/database.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '@user/user.entity';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [DatabaseModule, TypeOrmModule.forFeature([User])],
+  controllers: [AppController, UserController],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
