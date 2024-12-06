dd6ac036202ba5c7e589a842e461f33a00f68125
Jordan MacHattie <jwmhattie@gmail.com>
2024-12-06 17:06:49 -0400

    add navigation component

---
+++ apps/my-frontend/src/components/navigation.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+//TODO css
+
+const Navigation: React.FC = () => {
+  return (
+    <nav className="navigation">
+      <ul className="nav-list">
+        <li className="nav-item">
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/about" className="nav-link">
+            About Me
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/other" className="nav-link">
+            Other
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navigation;
