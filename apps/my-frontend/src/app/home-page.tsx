e4b21e8a3290f12d09272851e30a635b772a9f43
Jordan MacHattie <jwmhattie@gmail.com>
2024-12-06 17:07:03 -0400

    create home page

---
+++ apps/my-frontend/src/app/home-page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Navigation from "../components/navigation";
+
+const HomePage: React.FC = () => {
+  return (
+    <Navigation></Navigation>
+  )
+}
+
+export default HomePage;
